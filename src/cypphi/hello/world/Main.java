1c1e2994be6d98cb69cfb89816b389eba4c595b7
Cypphi <altcypphi@gmail.com>
2022-02-28 01:03:01 +0100

    part 1

---
+++ src/cypphi/hello/world/Main.java
@@ -0,0 +1,79 @@
+// java package
+package cypphi.hello.world;
+
+public class Main {
+    // Making sure true is actually true
+    public static boolean isTrue = true;
+    // An extra check to make sure isTrue is actually true
+    public static boolean isActuallyTrue = isTrue;
+
+    // Here I store all letters as strings so I can access them more easily.
+
+    // Lowercase letters
+    public static String a = "a";
+    public static String b = "b";
+    public static String c = "c";
+    public static String d = "d";
+    public static String e = "e";
+    public static String f = "f";
+    public static String g = "g";
+    public static String h = "h";
+    public static String i = "i";
+    public static String j = "j";
+    public static String k = "k";
+    public static String l = "l";
+    public static String m = "m";
+    public static String n = "n";
+    public static String o = "o";
+    public static String p = "p";
+    public static String q = "q";
+    public static String r = "r";
+    public static String s = "s";
+    public static String t = "t";
+    public static String u = "u";
+    public static String v = "v";
+    public static String w = "w";
+    public static String x = "x";
+    public static String y = "y";
+    public static String z = "z";
+
+    // Uppercase letters
+    public static String aUppercase = "A";
+    public static String bUppercase = "B";
+    public static String cUppercase = "C";
+    public static String dUppercase = "D";
+    public static String eUppercase = "E";
+    public static String fUppercase = "F";
+    public static String gUppercase = "G";
+    public static String hUppercase = "H";
+    public static String iUppercase = "I";
+    public static String jUppercase = "J";
+    public static String kUppercase = "K";
+    public static String lUppercase = "L";
+    public static String mUppercase = "M";
+    public static String nUppercase = "N";
+    public static String oUppercase = "O";
+    public static String pUppercase = "P";
+    public static String qUppercase = "Q";
+    public static String rUppercase = "R";
+    public static String sUppercase = "S";
+    public static String tUppercase = "T";
+    public static String uUppercase = "U";
+    public static String vUppercase = "V";
+    public static String wUppercase = "W";
+    public static String xUppercase = "X";
+    public static String yUppercase = "Y";
+    public static String zUppercase = "Z";
+
+
+    // main method to make the program and it's efficient code function
+    public static void main(String[] args) {
+        // Check if isTrue = true;
+        if (isTrue) {
+            if (isActuallyTrue) {
+                // print out "Hello World!"
+                System.out.println("Hello World!");
+            }
+        }
+    }
+}
